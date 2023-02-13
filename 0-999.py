e1f1e13f03cec5f02c21139b83b04abee41e55bc
AXiePimon <125174615+AXiePimon@users.noreply.github.com>
2023-02-14 00:07:28 +0800

    Add files via upload
    最新版，修复了无法保存小于号之后的文本问题

--- 0-999.py
+++ 0-999.py
@@ -5,7 +5,7 @@ from lxml import etree
 global x
 x=0
 urls=['https://dl1.wenku8.com/txtutf8/0/{}.txt'.format(i) for i in range(0,1000)]
-path=r'D:\novels\0-999\novel'
+path=r'D:\novels\novel'
 headers = {
     'Cookie':'OCSSID=4df0bjva6j7ejussu8al3eqo03',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
@@ -13,13 +13,13 @@ headers = {
 }
 def get_text(url,x):
     r=requests.get(url,headers=headers)
-    r.encoding='gdk'
+    r.encoding='UTF-8'
     selector=etree.HTML(r.text)
     title=str(x)
-    text=selector.xpath('//text()')
+
     #text2=selector.xpath('/html/body/pre/text()[2]')
     with open(path+title+'.txt','w',encoding='UTF-8') as f:
-        for i in text:
+        for i in str(r.text):
             f.write(i)
         #for i in text2:
             #f.write(i)#
@@ -29,4 +29,4 @@ if __name__ == "__main__":
         get_text(url,x)
         print(x,"finished")
         x+=1
-input("全部小说已下载完成，按回车键自动退出",x)
+
--- 1000-1999.py
+++ 1000-1999.py
@@ -5,7 +5,7 @@ from lxml import etree
 global x
 x=1000
 urls=['https://dl1.wenku8.com/txtutf8/1/{}.txt'.format(i) for i in range(1000,2000)]
-path=r'D:\novels\1000-1999\novel'
+path=r'D:\novels\novel'
 headers = {
     'Cookie':'OCSSID=4df0bjva6j7ejussu8al3eqo03',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
@@ -13,13 +13,13 @@ headers = {
 }
 def get_text(url,x):
     r=requests.get(url,headers=headers)
-    r.encoding='gdk'
+    r.encoding='UTF-8'
     selector=etree.HTML(r.text)
     title=str(x)
-    text=selector.xpath('//text()')
+
     #text2=selector.xpath('/html/body/pre/text()[2]')
     with open(path+title+'.txt','w',encoding='UTF-8') as f:
-        for i in text:
+        for i in str(r.text):
             f.write(i)
         #for i in text2:
             #f.write(i)#
